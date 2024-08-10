ad6a9864f8c3fb6d9223be1252b732683eb3ae3b
mojyack <mojyack@gmail.com>
2024-08-10 15:17:26 +0900

    server: add -k option

--- src/server.cpp
+++ src/server.cpp
@@ -6,15 +6,17 @@
 #include "protocol-helper.hpp"
 #include "server.hpp"
 #include "util/argument-parser.hpp"
+#include "util/file-io.hpp"
 #include "ws/misc.hpp"
 
 struct ServerArgs {
-    uint16_t port;
-    bool     help                   = false;
-    bool     verbose                = false;
-    bool     websocket_verbose      = false;
-    bool     websocket_dump_packets = false;
-    uint8_t  libws_debug_bitmap     = 0b11; // LLL_ERR | LLL_WARN
+    const char* session_key_secret_file = nullptr;
+    uint16_t    port                    = 0;
+    bool        help                    = false;
+    bool        verbose                 = false;
+    bool        websocket_verbose       = false;
+    bool        websocket_dump_packets  = false;
+    uint8_t     libws_debug_bitmap      = 0b11; // LLL_ERR | LLL_WARN
 
     static auto parse(const int argc, const char* const* argv, std::string_view program_name, uint16_t default_port) -> std::optional<ServerArgs>;
 };
@@ -24,6 +26,7 @@ auto ServerArgs::parse(const int argc, const char* const* const argv, std::strin
     auto parser = args::Parser<uint16_t, uint8_t>();
     parser.kwarg(&args.help, {"-h", "--help"}, {.arg_desc = "print this help message", .state = args::State::Initialized, .no_error_check = true});
     parser.kwarg(&args.port, {"-p"}, {"PORT", "port number to use", args::State::DefaultValue});
+    parser.kwarg(&args.session_key_secret_file, {"-k", "--key"}, {"FILE", "enable user verification with the secret file", args::State::Initialized});
     parser.kwarg(&args.verbose, {"-v"}, {.arg_desc = "enable signaling server debug output", .state = args::State::Initialized});
     parser.kwarg(&args.websocket_verbose, {"-wv"}, {.arg_desc = "enable websocket debug output", .state = args::State::Initialized});
     parser.kwarg(&args.websocket_dump_packets, {"-wd"}, {.arg_desc = "dump every websocket packets", .state = args::State::Initialized});
@@ -41,6 +44,10 @@ auto run(const int argc, const char* const* const argv,
          std::unique_ptr<ws::server::SessionDataInitializer> session_initer,
          const char* const                                   protocol) -> bool {
     unwrap_ob(args, ServerArgs::parse(argc, argv, protocol, default_port));
+    if(args.session_key_secret_file != nullptr) {
+        unwrap_ob(secret, read_file(args.session_key_secret_file), "failed to read session key secret file");
+        server.session_key.emplace(secret);
+    }
     server.verbose = args.verbose;
 
     auto& wsctx   = server.websocket_context;
--- src/server.hpp
+++ src/server.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include "protocol-helper.hpp"
+#include "session-key.hpp"
 #include "ws/server.hpp"
 
 struct Session {
+    bool activated = false;
+
     virtual auto handle_payload(std::span<const std::byte> payload) -> bool = 0;
     virtual ~Session() {}
 };
 
 struct Server {
-    ws::server::Context websocket_context;
-    bool                verbose = false;
+    ws::server::Context       websocket_context;
+    std::optional<SessionKey> session_key;
+    bool                      verbose = false;
 
     template <class... Args>
     auto send_to(lws* const wsi, const uint16_t type, const uint32_t id, Args... args) -> bool {
