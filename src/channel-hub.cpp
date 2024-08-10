ad84427d306132390601a34d0fd6b8902235039a
mojyack <mojyack@gmail.com>
2024-08-10 21:24:10 +0900

    channel-hub: use print

--- src/channel-hub.cpp
+++ src/channel-hub.cpp
@@ -56,9 +56,9 @@ auto ChannelHubSession::handle_payload(const std::span<const std::byte> payload)
 
     if(header.type == ::p2p::proto::Type::ActivateSession) {
         const auto cert = p2p::proto::extract_last_string<proto::Register>(payload);
-        PRINT("received activate session");
+        print("received activate session");
         assert_b(activate(*server, cert), "failed to verify user certificate");
-        PRINT("session activated");
+        print("session activated");
         goto finish;
     } else {
         assert_b(activated, estr[Error::NotActivated]);
@@ -71,28 +71,28 @@ auto ChannelHubSession::handle_payload(const std::span<const std::byte> payload)
         return true;
     case proto::Type::Register: {
         const auto name = p2p::proto::extract_last_string<proto::Register>(payload);
-        PRINT("received channel register request name:", name);
+        print("received channel register request name:", name);
 
         assert_b(!name.empty(), estr[Error::EmptyChannelName]);
         assert_b(server->channels.find(name) == server->channels.end(), estr[Error::ChannelFound]);
 
-        PRINT("channel ", name, " registerd");
+        print("channel ", name, " registerd");
         server->channels.insert(std::pair{name, Channel{std::string(name), wsi}});
     } break;
     case proto::Type::Unregister: {
         const auto name = p2p::proto::extract_last_string<proto::Unregister>(payload);
-        PRINT("received channel unregister request name: ", name);
+        print("received channel unregister request name: ", name);
 
         const auto it = server->channels.find(name);
         assert_b(it != server->channels.end(), estr[Error::ChannelNotFound]);
         auto& channel = it->second;
         assert_b(channel.wsi == wsi, estr[Error::SenderMismatch]);
 
-        PRINT("unregistering channel ", channel.name);
+        print("unregistering channel ", channel.name);
         server->channels.erase(it);
     } break;
     case proto::Type::GetChannels: {
-        PRINT("received channel list request");
+        print("received channel list request");
         auto payload = std::vector<std::byte>();
         for(auto it = server->channels.begin(); it != server->channels.end(); it = std::next(it)) {
             const auto& name      = it->second.name;
@@ -106,7 +106,7 @@ auto ChannelHubSession::handle_payload(const std::span<const std::byte> payload)
     } break;
     case proto::Type::PadRequest: {
         const auto name = p2p::proto::extract_last_string<proto::PadRequest>(payload);
-        PRINT("received pad request for channel: ", name);
+        print("received pad request for channel: ", name);
 
         // check if another request is pending
         for(auto i = server->pending_sessions.begin(); i != server->pending_sessions.end(); i = std::next(i)) {
@@ -122,7 +122,7 @@ auto ChannelHubSession::handle_payload(const std::span<const std::byte> payload)
         server->pending_sessions.insert({id, this});
     } break;
     case proto::Type::PadRequestResponse: {
-        PRINT("received pad request response");
+        print("received pad request response");
 
         unwrap_pb(packet, p2p::proto::extract_payload<proto::PadRequestResponse>(payload));
         const auto pad_name = p2p::proto::extract_last_string<proto::PadRequestResponse>(payload);
@@ -132,7 +132,7 @@ auto ChannelHubSession::handle_payload(const std::span<const std::byte> payload)
         const auto requester = requester_it->second;
         server->pending_sessions.erase(header.id);
 
-        PRINT("sending pad name ok: ", packet.ok, " pad_name: ", pad_name);
+        print("sending pad name ok: ", packet.ok, " pad_name: ", pad_name);
         assert_b(server->send_to(requester->wsi, proto::Type::PadRequestResponse, 0, packet.ok, pad_name));
     } break;
     default: {
@@ -154,14 +154,14 @@ struct SessionDataInitializer : ws::server::SessionDataInitializer {
     }
 
     auto init(void* const ptr, lws* wsi) -> void override {
-        PRINT("session created: ", ptr);
+        print("session created: ", ptr);
         auto& session  = *new(ptr) ChannelHubSession();
         session.server = server;
         session.wsi    = wsi;
     }
 
     auto deinit(void* const ptr) -> void override {
-        PRINT("session destroyed: ", ptr);
+        print("session destroyed: ", ptr);
         auto& session = *std::bit_cast<ChannelHubSession*>(ptr);
 
         // remove corresponding channels
