da165312c982b071c333f64121998151f3caf654
mojyack <mojyack@gmail.com>
2024-08-10 16:15:56 +0900

    example: channel-hub-client-test: support user certificate

--- example/channel-hub-client-test.cpp
+++ example/channel-hub-client-test.cpp
@@ -1,5 +1,8 @@
 #include "macros/unwrap.hpp"
 #include "p2p/channel-hub-client.hpp"
+#include "util/argument-parser.hpp"
+#include "util/file-io.hpp"
+#include "util/span.hpp"
 
 namespace {
 class ChannelHubSender : public p2p::chub::ChannelHubSender {
@@ -14,15 +17,32 @@ class ChannelHubSender : public p2p::chub::ChannelHubSender {
   public:
 };
 
-auto run() -> bool {
+auto run(const int argc, const char* const* const argv) -> bool {
+    auto cert_file = (const char*)(nullptr);
+    auto help      = false;
+
+    auto parser = args::Parser<uint16_t, uint8_t>();
+    parser.kwarg(&help, {"-h", "--help"}, {.arg_desc = "print this help message", .state = args::State::Initialized, .no_error_check = true});
+    parser.kwarg(&cert_file, {"-k"}, {"CERT_FILE", "use user certificate", args::State::Initialized});
+    if(!parser.parse(argc, argv) || help) {
+        print("usage: channel-hub-client-test ", parser.get_help());
+        return true;
+    }
+
+    auto user_cert = std::string();
+    if(cert_file != nullptr) {
+        unwrap_ob(cert, read_file(cert_file));
+        user_cert = from_span(cert);
+    }
+
     const auto channel_hub = p2p::wss::ServerLocation{"localhost", 8081};
 
     auto sender    = ChannelHubSender();
     sender.verbose = true;
     sender.set_ws_debug_flags(true, true);
     auto receiver = p2p::chub::ChannelHubReceiver();
-    assert_b(sender.start(channel_hub));
-    assert_b(receiver.start(channel_hub));
+    assert_b(sender.start(channel_hub, user_cert));
+    assert_b(receiver.start(channel_hub, user_cert));
 
     assert_b(sender.register_channel("room-1-audio"));
     assert_b(sender.register_channel("room-1-video"));
@@ -36,6 +56,6 @@ auto run() -> bool {
 }
 } // namespace
 
-auto main() -> int {
-    return run() ? 0 : 1;
+auto main(const int argc, const char* const* const argv) -> int {
+    return run(argc, argv) ? 0 : 1;
 }
